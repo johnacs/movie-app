e646a4d8cbcbcf3a3b9442e30c160a27b7a8b543
johnacs <johnacsyen@gmail.com>
2020-09-10 11:41:07 +0800

    gather material core to single import

--- src/components/GenreSelect.js
+++ src/components/GenreSelect.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-
+import { MenuItem, FormControl, Select } from '@material-ui/core/';
 import { useMovies } from '../MovieContext';
 import { withRouter } from 'react-router-dom';
 
